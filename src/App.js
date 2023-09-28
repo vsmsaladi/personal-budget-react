42bb57091805e0c6062bf90e175291e71c0b2b58
Santosh Mohan Saladi <vsaladi@uncc.edu>
2023-09-28 14:23:54 -0400

    Brought personal Budget code in

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Menu from './Menu/Menu';
+import HomePage from './Homepage/HomePage';
+import Hero from './Hero/Hero';
+import Footer from './Footer/Footer';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import AboutPage from './AboutPage/AboutPage';
+import LoginPage from './LoginPage/LoginPage';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Menu />
+      <Hero />
+      <div className='mainContainer'>
+        <Routes>
+          <Route path='/' element={<HomePage/>}/>
+          <Route path='/about' element={<AboutPage/>}/>
+          <Route path='/login' element={<LoginPage/>}/>
+
+        </Routes>
+      </div>
+      <Footer />
+      </Router>
+
   );
 }
 
---
+++ src/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+function Footer() {
+    return (
+        <footer className="bottom">
+            <div className="center">
+                Changes done by Santosh Mohan.
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
